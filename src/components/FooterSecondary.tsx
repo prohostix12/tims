0cfdb60f0cb490af210d0cb338a37e43f47be1dc
Archana <archanap13u@gmail.com>
2026-05-13 17:18:11 +0530

    Update FooterSecondary component

--- src/components/FooterSecondary.tsx
+++ src/components/FooterSecondary.tsx
@@ -33,19 +33,7 @@ const FooterSecondary = () => {
           </div>
         </div>
 
-        {/* Explore Section */}
-        <div className={styles.footerCol}>
-          <h3>Explore</h3>
-          <ul className={styles.linkList}>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/about">About</Link></li>
-            <li><Link href="/blog">Blog</Link></li>
-            <li><Link href="/news">News</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
-            <li><Link href="/terms">Terms and Conditions</Link></li>
-            <li><Link href="/privacy">Privacy Policy</Link></li>
-          </ul>
-        </div>
+
 
         {/* Head Office Section */}
         <div className={styles.footerCol}>
@@ -83,40 +71,10 @@ const FooterSecondary = () => {
             <input type="email" placeholder="Enter your email" className={styles.emailInput} />
             <button className={styles.subscribeBtn}>Subscribe</button>
           </div>
-          <div className={styles.badges}>
-            <span className={styles.badge}>UGC-DEB</span>
-            <span className={styles.badge}>NAAC</span>
-            <span className={styles.badge}>AICTE</span>
-            <span className={styles.badge}>AIU</span>
-          </div>
+
         </div>
 
-        {/* Edapal Office Section */}
-        <div className={styles.footerCol}>
-          <h3>Edapal Office</h3>
-          <div className={styles.contactInfo}>
-            <div className={styles.contactItem}>
-              <Mail className={styles.icon} size={18} />
-              <div className={styles.contactText}>
-                <a href="mailto:info@findyouruniversity.com">info@findyouruniversity.com</a>
-              </div>
-            </div>
-            <div className={styles.contactItem}>
-              <Phone className={styles.icon} size={18} />
-              <div className={styles.contactText}>
-                <a href="tel:+919526387777">+91 9526387777</a>
-              </div>
-            </div>
-            <div className={styles.contactItem}>
-              <MapPin className={styles.icon} size={18} />
-              <div className={styles.contactText}>
-                2nd floor Al madeela complex<br />
-                Calicut road Edappal 679576<br />
-                MALAPPURAM DT Kerala
-              </div>
-            </div>
-          </div>
-        </div>
+
       </div>
 
       <div className={styles.footerBottom}>
